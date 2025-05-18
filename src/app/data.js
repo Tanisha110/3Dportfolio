62ad22840e4cf9d1ba514902df13ac35b65854ca
Tanisha <tanishasonkar110@gmail.com>
2025-05-18 13:33:05 +0530

    data changes

--- src/app/data.js
+++ src/app/data.js
@@ -2,6 +2,7 @@ import p1 from '../../public/background/p1.png';
 import p2 from '../../public/background/p2.png';
 import p3 from '../../public/background/p3.png';
 import p4 from '../../public/background/p4.png';
+import p5 from '../../public/background/p5.png';
 export const projectsData = [
   {
     id: 1,
@@ -12,24 +13,38 @@ export const projectsData = [
   },
   {
     id: 2,
-    name: "FUZZY AUTOCOMPLETE SYSTEM",
-    description: "A dynamic fuzzy autocomplete system with enhanced phrase matching and user-driven suggestions having trie data structure implementation. It leverages fuzzy string matching algorithms to provide accurate and relevant suggestions as users type.The project includes a file upload feature to expand the autocomplete dictionary, ranks suggestions based on frequency and edit distance, and offers pagination for large result sets. ",
-    demoLink: "https://github.com/Tanisha110/Trie_implementation",
-    imageUrl: p2
+    name: " Ink2Pic Website",
+    description: "Ink2Pic is a text-to-image conversion web app designed to transform written content into compelling visuals. Built using React.js for the frontend and integrated with a custom image generation backend, it offers a seamless user experience with a clean, gradient-themed UI. The app uses advanced prompt parsing and styling logic to render high-quality images from user input. Developed as a developer portfolio project, Ink2Pic highlights real-world application of frontend/backend integration, responsive design, and dynamic image rendering.",
+    demoLink: "https://github.com/Tanisha110/imagify",
+    imageUrl: p5
   },
   {
     id: 3,
+    name: " 3D Interactive Portfolio Website",
+    description: "A visually immersive portfolio built using Next.js and Three.js, combining modern web development with 3D interactivity. The site showcases projects through smooth navigation, responsive design, and a creative user experience that reflects both technical skill and personal style.",
+    demoLink: "https://trading-website-eta.vercel.app/",
+    imageUrl: p4
+  },
+  {
+    id: 4,
+    name: " Trading Website",
+    description: "A Expert Training Hub is a responsive, modern web portfolio. It was developed by converting a Figma design into a fully functional interface using React.js for component-based structure and Tailwind CSS for utility-first styling. The site features an interactive carousel, structured content sections, and a polished contact form with integrated icons using Lucide React, ensuring both performance and aesthetic appeal.",
+    demoLink: "https://tanishasonkarportfolio.vercel.app/",
+    imageUrl: p4
+  },
+  {
+    id: 5,
     name: "Audio Source Separation & Visualization",
     description: "A web-based application that performs Blind Source Separation (BSS) using the FastICA algorithm. Users upload two mixed audio files, and the system separates them into their original source signals. It features audio playback, signal visualizations, and comparison plots. The backend is built with FastAPI and scikit-learn, while an additional Jupyter notebook explores a custom FastICA implementation from scratch for deeper insight.",
     demoLink: "https://github.com/Tanisha110/dc",
     imageUrl: p3
   },
   {
-    id: 4,
-    name: " 3D Interactive Portfolio Website",
-    description: "A visually immersive portfolio built using Next.js and Three.js, combining modern web development with 3D interactivity. The site showcases projects through smooth navigation, responsive design, and a creative user experience that reflects both technical skill and personal style.",
-    demoLink: "https://tanishasonkarportfolio.vercel.app/",
-    imageUrl: p4
+    id: 6,
+    name: "FUZZY AUTOCOMPLETE SYSTEM",
+    description: "A dynamic fuzzy autocomplete system with enhanced phrase matching and user-driven suggestions having trie data structure implementation. It leverages fuzzy string matching algorithms to provide accurate and relevant suggestions as users type.The project includes a file upload feature to expand the autocomplete dictionary, ranks suggestions based on frequency and edit distance, and offers pagination for large result sets. ",
+    demoLink: "https://github.com/Tanisha110/Trie_implementation",
+    imageUrl: p2
   },
 ];
 
